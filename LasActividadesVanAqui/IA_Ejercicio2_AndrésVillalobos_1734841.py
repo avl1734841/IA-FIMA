0d926f1d7a8299bd19455e249a0f3fd2bda0ca81
avl1734841 <90301661+avl1734841@users.noreply.github.com>
2021-09-08 00:41:44 -0500

    Add files via upload

---
+++ LasActividadesVanAqui/IA_Ejercicio2_AndrésVillalobos_1734841.py
@@ -0,0 +1,33 @@
+"""
+Actividad 2: Ejercicios Introduccion Python
+Andrés Villalobos López 1734841
+Sabado N1
+"""
+msj = "Ejercicio 2"
+print(msj)
+
+#utlizando input introduces los numeros enteros y flotantes
+x = input("Introduce primer numero entero: ")
+y = input("Introduce segundo numero entero: ")
+z = input("Introduce primer numero flotante: ")
+w = input("Introduce segundo numero flotante: ")
+
+#Utilizando print y el comando int espesificas que te den numeros enteros
+print("Suma Enteros: ")
+print(int(x) + int(y))
+print("Resta Enteros: ")
+print(int(x) - int(y))
+print("Multiplicación Enteros: ")
+print(int(x) * int(y))
+print("Division Enteros: ")
+print(int(x) / int(y))
+
+#Utilizando print y el comando float espesificas que te den numeros flotantes
+print("Suma Flotante: ")
+print(float(z) + float(w))
+print("Resta Flotante: ")
+print(float(z) - float(w))
+print("Multiplicación Flotante: ")
+print(float(z) * float(w))
+print("Division Flotante: ")
+print(float(z) / float(w))
