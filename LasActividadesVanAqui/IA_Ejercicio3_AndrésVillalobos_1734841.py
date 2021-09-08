979a8d6c3da040cf4bf6a7007eb254ee8461a3b6
avl1734841 <90301661+avl1734841@users.noreply.github.com>
2021-09-08 02:35:41 -0500

    Add files via upload

---
+++ LasActividadesVanAqui/IA_Ejercicio3_AndrésVillalobos_1734841.py
@@ -0,0 +1,31 @@
+"""
+Actividad 2: Ejercicios Introduccion Python
+Andrés Villalobos López 1734841
+Sebaado N1
+"""
+msg = "Ejercicio 3"
+print(msg)
+
+print("AND")
+#utilizando el comando and, la salida es True si ambos operadores dan True
+x = 10
+print(x>9 and x<15)
+
+x = 25
+print(x>10 and x<9)
+
+print("OR")
+#utilizando el comando or, la salida es True si alguno de los operadores da True
+x = 58
+print(x>25 or x<70)
+
+x = 20
+print(x>10 or x<30)
+
+print("NOT")
+#utilizando el comando not, la salida es False si ambos operadores dan True
+x = 15
+print(not(x>9 and x<20))
+
+x = 30
+print(not(x>10 and x<40))
