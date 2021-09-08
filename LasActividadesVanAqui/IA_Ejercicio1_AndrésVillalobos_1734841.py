db7e799d388788368735ea41beea84f636f6930f
avl1734841 <90301661+avl1734841@users.noreply.github.com>
2021-09-08 00:39:32 -0500

    Add files via upload

---
+++ LasActividadesVanAqui/IA_Ejercicio1_AndrésVillalobos_1734841.py
@@ -0,0 +1,17 @@
+"""
+Actividad 2: Ejercicios introduccion Python
+Andrés Villalobos López 1734841
+Sabado N1
+"""
+msg = "Ejercicio 1"
+print(msg)
+
+#Usando el comando input para que te pregunte los valores en el programa
+nombre = input("Introducir nombre: ")
+edad = input("Introducir edad: ")
+frase = input("Introducir tu frase favorita: ")
+autor = input("Autor de la frase: ")
+
+#Utilizando print sumando texto mas las variables puedes crear una oracion
+print("Hola mi nombre es " + nombre + ", tengo " + edad + " años " +
+      " y mi frase favorita es: " + frase + " dicha por el autor " + autor)
