2db7599e26d02a89d9e8727e845005e753b793a5
avl1734841 <90301661+avl1734841@users.noreply.github.com>
2021-09-11 18:23:23 -0500

    Add files via upload

---
+++ Primer_Examen_Parcial_Sabatinos/primer_examen_parcial.py
@@ -0,0 +1,13 @@
+"""
+Primer examen parcial
+Andrés Villalobos López
+1734841
+Sabatinos N1
+"""
+print("Primer Examen Parcial")
+
+#usamos el comando int para que el numero sea entero
+x = int(input("Inserte un numero al azar: "))
+
+#utilizamos el comando print con la formula dada que cumple con lo pedido
+print(x * (x + 1) // 2) 
